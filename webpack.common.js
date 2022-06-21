4eb8f0469ba6ee2f58852de786b45ab84f61e859
reza-nazari <47356035+reza-nazari@users.noreply.github.com>
2022-06-22 00:23:34 +0430

    add glob

--- webpack.common.js
+++ webpack.common.js
@@ -3,45 +3,16 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const glob = require('glob');
-
-function getEntry() {
-    const entry = {};
-    glob.sync('./src/pages/**/index.ts').forEach((file) => {
-        const name = file.match(/\/pages\/(.+)\/index.ts/)[1];
-        entry[name] = file;
-        console.log(entry)
-    });
-    return entry;
-}
-
-function getHtmlTemplate() {
-  return glob
-    .sync('./src/pages/**/index.html')
-    .map((file) => {
-      return { name: file.match(/\/pages\/(.+)\/index.html/)[1], path: file };
-    })
-    .map(
-      (template) =>
-        new HtmlWebpackPlugin({
-          template: template.path,
-          chunks: [template.name.toString()],
-          filename: `${template.name}/index.html`,
-        })
-    );
-}
-
 
 module.exports = {
     entry: {
-        index: './src/index.ts',
-        ...getEntry(),
+        layout: './src/js/layout.js',
+        index: './src/js/index.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/[name].js',
     },
-    devtool: 'inline-source-map',
     stats: {
         assets: true,
         children: true,
@@ -49,11 +20,6 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
             {
                 test: /\.js$/,
                 use: "babel-loader",
@@ -122,9 +88,6 @@ module.exports = {
             },
         ]
     },
-    resolve: {
-        extensions: ["", ".webpack.js", ".web.js", ".js", ".ts", ".html"]
-    },
     performance: {
         maxEntrypointSize: 700000,
         maxAssetSize: 700000,
@@ -136,14 +99,14 @@ module.exports = {
             filename: 'index.html',
             template: 'src/index.html',
             inject: 'body',
-            chunks: ["index"],
+            chunks: ["index", 'layout'],
             favicon: './src/assets/favicon.ico'
         }),
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
             filename: "[name].css"
-        }),
+          }),
         new CleanWebpackPlugin(),
-    ].concat(...getHtmlTemplate()),
+    ],  
 };
